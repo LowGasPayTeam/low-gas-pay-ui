9914e3ae0ab6c685a34525f3875adcfd8bc901f2
cryptomodeert <cryptomodeert@gmail.com>
2022-06-02 23:52:31 +0800

    feat: chagne dockerfile

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   async rewrites() { 
     return [ 
-     //接口请求 前缀带上/api-text/
       { source: '/api/:path*', destination: `http://47.242.89.124:9999/api/:path*` }, 
 
     ]
